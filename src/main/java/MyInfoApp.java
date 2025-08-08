d13910b48c7af1302d5369d45e1f691039484e40
sanghee Shin <113161106+shinsanghee@users.noreply.github.com>
2025-08-08 10:37:55 +0900

    Delete MyInfoApp.java

--- src/main/java/MyInfoApp.java
+++
@@ -1,9 +0,0 @@
-
-public class MyInfoApp {
-	
-	public static void main(String[] args) {
-	System.out.println("Kitae Hwang\n");
-	System.out.println(	"20 years old\n"); 
-	System.out.println("Department of Computer Engineering\n");
-	}
-}
