2f6a511c10f9e88f180a2ed3320514c4ab10b050
sanghee Shin <113161106+shinsanghee@users.noreply.github.com>
2025-08-08 10:53:01 +0900

    Delete ex00 directory

--- ex00/src/main/java/org/zerock/sample/LogExample.java
+++
@@ -1,5 +0,0 @@
-package org.zerock.sample;
-
-public class LogExample {
-	private static final java.util.logging.Logger log  = java.util.logging.Logger.getLogger(LogExample.class.getName());
-}
--- ex00/src/main/java/org/zerock/sample/SampleDTOList.java
+++
@@ -1,9 +0,0 @@
-package org.zerock.sample;
-
-@Data
-public class SampleDTOList {
-	
-	private List<SampleDTO> list;
-	
-	public 
-}
