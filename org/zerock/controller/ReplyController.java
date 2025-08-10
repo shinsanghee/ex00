679873c97e4cf9ecee291872d3abe83fd21fca2f
sanghee Shin <113161106+shinsanghee@users.noreply.github.com>
2025-08-10 12:08:33 +0900

    Update ReplyController.java

--- org/zerock/controller/ReplyController.java
+++ org/zerock/controller/ReplyController.java
@@ -1,49 +1,93 @@
 package org.zerock.controller;
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.zerock.domain.Criteria;
+import org.zerock.service.ReplyService;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@RequestMapping("/replies/")
+@RestController
+@Log4j
+@AllArgsConstructor
 public class ReplyController {
 
-	@GetMapping(value = "/pages/{bno}/{page}",
-		produces = {
-			MediaType.APPLICATION_XML_VALUE,
-			MediaType.APPLICATION_JSON_UTF8_VALUE })
+	private ReplyService service;
+}
+
+//구멍코딩단, p393 부분부터
+
+	@PostMapping(value = "/new", 
+			consumes = "application/json", 
+			produces = { MediaType.TEXT_PLAIN_VALUE })
+	public ResponseEntity<String> create(@RequestBody ReplyVO vo) {
+
+		log.info("ReplyVO: " + vo);
+
+		int insertCount = service.register(vo);
+
+		log.info("Reply INSERT COUNT: " + insertCount);
+
+		return insertCount == 1 
+		? new ResponseEntity<>("success", HttpStatus.OK)
+	    : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		// 삼항 연산자 처리
+	}
+
+	@GetMapping(value='/pages/{bno}/{page}", 
+		produces= {
+			 MediaType.APPLICATION_Xml_VALUE,
+			 MediaType.APPLICATION_JSON_UTF8_VALUE })
 	public ResponseEntity<List<ReplyVO>> getList(
-		@PathVariable("page") int page,
-		@PathVariable("bno") Long bno) {
+			@PathVariable("page") int page, 
+			@PathVariable("bno") Long bno) {
 		
-	  log.info("getList..................");
-	  Criteria cri = new Criteria(page, 10);
-	  log.info(cri);
-	  
-	return new ResponseEntity<>(service.getList(cri, bno), HttpSttatus.OK);
-	
-	@GetMapping(value = "/pages/{rno",
-			produces = { MediaType.APPLICATION_XML_VALUE,
-						 MediaType.APPLICATION_JSON_UTF8_VALUE })
-		public ResponseEntity<ReplyVO> get(@PathVariable("rno")Long rno)  {
+		log.info("getList................................");
+		Criteria cri = new Criteria(page, 10);
+
+		log.info(cri);
 		
-			log.info("get: "  rno);
+		return new ResponseEntity<>(service.getList(cri, bno), HttpStatus.OK);
+	}
+
+	@GetMapping(value = "/{rno}",
+		produces = { MediaType.APPLICATION_XML_VALUE, 
+			         MediaType.APPLICATION_JSON_UTF8_VALUE }) 
+		public ResponseEntity<ReplyVO> get(@PathVariable("rno") Long rno) {
+			 
+		log.info("get: " + rno);
+		
+		return new ResponseEntity<>(service.get(rno), HttpStatus.OK);
+
+	}
+
+	@DeleteMapping(value= "/{rno}" ,produces = { MediaType.TEXT_PLAIN_VALUE }) 
+	public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
 			
-			return new ResponseEntity<>(service.get(gno), HttpStatus.OK);
-		}
-	
-	@DeleteMapping(value= "/{rno}", produces = { MediaType.TEXT_PLAIN_VALUE })
-	public ResponseEntity<String> remove(@PathVariable("rno") Long rno){
-		
 		log.info("remove: " + rno);
 		
 		return service.remove(rno) == 1
-		  ? new ResponseEntity<>("success", HttpStatus.OK)
+		  ? new ResponseEntity<>("success", HttpStatus.OK) 
 		  : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+			 
 	}
+
+	@RequestMapping(method = { RequestMethod.PUT, RequestMethod.PATCH }, 
+			value = "/{rno}", 
+			consumes = "application/json", 
+			produces = { MediaType.TEXT_PLAIN_VALUE })
+	public ResponseEntity<String> modify(
+			@RequestBody ReplyVO vo, 
+			@PathVariable("rno") Long rno) {
 	
-	@RequestMapping(method = {})
+		vo.setRno(rno);
+			 
+		log.info("rno: " + rno);
+			 
+	    log.info("modify: " + vo);
+	    
+		return service.modify(vo) == 1
+				  ? new ResponseEntity<>("success", HttpStatus.OK)
+				  : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+			 
+	}
